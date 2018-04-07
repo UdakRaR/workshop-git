19772bb7838557c9b1176c20e9b1920f858bff8b
kyllu <guilhermemolyveira@hotmail.com>
2018-04-07 11:09:15 -0300

    implement subtract

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-module.exports = class Calculator{
+﻿module.exports = class Calculator{
 
 	print(a,b){
 		return String("a = "+a, "b = "+b);
 	}
+
+        subtract(a,b){
+		return a-b;
+	}
+
 
 	// implementar as outras funções aqui
 }
